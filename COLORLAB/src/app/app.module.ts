313c572b2361422b2f13bf7725fea3bcfc1d9e98
navi-med <navi.medrano@cetys.edu.mx>
2022-11-25 21:18:42 -0800

    modificado los paths

--- COLORLAB/src/app/app.module.ts
+++ COLORLAB/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {prueba} from './prueba/prueba.component';
-import { HelloWorldComponent } from './hello-world/hello-world.component'
+import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { HeaderComponent } from './Components/header/header.component'
 @NgModule({
   declarations: [
     AppComponent,
     prueba,
-    HelloWorldComponent
+    HelloWorldComponent,
+    FooterComponent,
+    HeaderComponent
 
   ],
   imports: [
