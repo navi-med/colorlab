aebb62042ed7a80b8744b4e2cba64dfc70a5613b
navi-med <navi.medrano@cetys.edu.mx>
2022-11-26 03:59:16 -0800

    header listo

--- COLORLAB/src/app/app.module.ts
+++ COLORLAB/src/app/app.module.ts
@@ -3,15 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {prueba} from './prueba/prueba.component';
-import { HelloWorldComponent } from './hello-world/hello-world.component';
+
 import { FooterComponent } from './Components/footer/footer.component';
 import { HeaderComponent } from './Components/header/header.component'
 @NgModule({
   declarations: [
     AppComponent,
-    prueba,
-    HelloWorldComponent,
+
     FooterComponent,
     HeaderComponent
 
