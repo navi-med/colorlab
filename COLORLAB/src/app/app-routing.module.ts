bc4a43f7be93b09de06e1dd9e621706cfa306a83
navi-med <navi.medrano@cetys.edu.mx>
2022-12-04 05:10:14 -0800

    header, footer, home, about (started)

--- COLORLAB/src/app/app-routing.module.ts
+++ COLORLAB/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './Components/about/about.component';
+import { HomePageComponent } from './Components/home-page/home-page.component';
+const routes: Routes = [
+{path:'', component: HomePageComponent},
 
-const routes: Routes = [];
+{path:'/about', component: AboutComponent}
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- COLORLAB/src/app/app.module.ts
+++ COLORLAB/src/app/app.module.ts
@@ -3,17 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {prueba} from './prueba/prueba.component';
-import { HelloWorldComponent } from './hello-world/hello-world.component';
+
 import { FooterComponent } from './Components/footer/footer.component';
-import { HeaderComponent } from './Components/header/header.component'
+import { HeaderComponent } from './Components/header/header.component';
+import { HomePageComponent } from './Components/home-page/home-page.component';
+import { AboutComponent } from './Components/about/about.component'
 @NgModule({
   declarations: [
     AppComponent,
-    prueba,
-    HelloWorldComponent,
+
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomePageComponent,
+    AboutComponent
+    
 
   ],
   imports: [
